96f86c9bd727bd0916f535542d8e1db5e699d2d7
Germán Carrillo <gcarrillo@linuxmail.org>
2020-07-27 20:07:44 -0500

    Add ETL models for 3.14 (the field mapper widget was migrated to C++)

--- AppendFeaturesToLayer/append_features_to_layer_plugin.py
+++ AppendFeaturesToLayer/append_features_to_layer_plugin.py
@@ -20,7 +20,7 @@ import os.path
 import glob
 import shutil
 
-from qgis.core import QgsApplication, QgsProcessingModelAlgorithm
+from qgis.core import QgsApplication, QgsProcessingModelAlgorithm, Qgis
 
 from processing.modeler.ModelerUtils import ModelerUtils
 from .processing.etl_load_provider import ETLLoadAlgorithmProvider
@@ -56,7 +56,24 @@ class AppendFeaturesToLayerPlugin:
         basepath = os.path.dirname(os.path.abspath(__file__))
         plugin_models_dir = os.path.join(basepath, "processing", "models")
 
-        for filename in glob.glob(os.path.join(plugin_models_dir, '*.model3')):
+        filenames = list()
+
+        # Go for subfolders.
+        # We store models that depend on QGIS versions in folders like "314" (for QGIS 3.14.x)
+        # This was initially needed for the FieldMapper input, which was migrated to C++ in QGIS 3.14
+        qgis_major_version = str(Qgis.QGIS_VERSION_INT)[:3]
+        qgis_major_version_path = os.path.join(plugin_models_dir, qgis_major_version)
+
+        if not os.path.isdir(qgis_major_version_path):
+            # No folder for this version (e.g., unit tests on QGIS-dev), so let's find the most recent version
+            subfolders = [sf.name for sf in os.scandir(plugin_models_dir) if sf.is_dir()]
+            if subfolders:
+                qgis_major_version_path = os.path.join(plugin_models_dir, max(subfolders))
+
+        for filename in glob.glob(os.path.join(qgis_major_version_path, '*.model3')):
+            filenames.append(filename)
+
+        for filename in filenames:
             alg = QgsProcessingModelAlgorithm()
             if not alg.fromFile(filename):
                 print("ERROR: Couldn't load model from {}".format(filename))
