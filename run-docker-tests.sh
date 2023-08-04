1aa4cccbb9e38d5ad7da3533a2b83458f441f175
Germán Carrillo <gcarrillo@linuxmail.org>
2023-08-03 22:00:01 -0500

    [tests] New multi_polygon PG table, utils method to drop all DB objects

--- run-docker-tests.sh
+++ run-docker-tests.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 pushd /usr/src/
-xvfb-run nose2-3
+#xvfb-run nose2-3
 #xvfb-run nose2-3 tests.test_pg_table_pg_table
 #xvfb-run nose2-3 tests.test_table_table
 #xvfb-run nose2-3 tests.test_source_simple_polygons
+xvfb-run nose2-3 tests.test_spatial_and_non_spatial
 popd
