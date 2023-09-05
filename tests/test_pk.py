667c4ae4db1631140372178f51808fcdea6184e5
Germán Carrillo <gcarrillo@linuxmail.org>
2023-09-05 19:23:03 +0200

    [tests] First part of PG test update PK

--- tests/test_pk.py
+++ tests/test_pk.py
@@ -11,7 +11,15 @@ from qgis.testing.mocked import get_iface
 import processing
 
 from tests.utils import (CommonTests,
-                         APPENDED_COUNT, UPDATED_COUNT, SKIPPED_COUNT, get_test_file_copy_path, get_test_path)
+                         APPENDED_COUNT,
+                         UPDATED_COUNT,
+                         SKIPPED_COUNT,
+                         get_test_file_copy_path,
+                         get_test_path,
+                         get_qgis_pg_layer,
+                         prepare_pg_db_1,
+                         drop_all_tables,
+                         PG_BD_1)
 
 start_app()
 
@@ -19,12 +27,13 @@ start_app()
 class TestTablePK(unittest.TestCase):
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         print('\nINFO: Set up TestTablePK')
         from AppendFeaturesToLayer.append_features_to_layer_plugin import AppendFeaturesToLayerPlugin
-        self.plugin = AppendFeaturesToLayerPlugin(get_iface)
-        self.plugin.initGui()
-        self.common = CommonTests()
+        cls.plugin = AppendFeaturesToLayerPlugin(get_iface)
+        cls.plugin.initGui()
+        cls.common = CommonTests()
+        prepare_pg_db_1()
 
     def test_append_update_PKs(self):
         print('\nINFO: Validating avoiding to set/update PKs...')
@@ -70,10 +79,59 @@ class TestTablePK(unittest.TestCase):
         self.assertEqual(res[UPDATED_COUNT], 3)
         self.assertIsNone(res[SKIPPED_COUNT])
 
+    def test_append_update_PKs_pg(self):
+        print('\nINFO: Validating avoiding to set/update PKs in PG...')
+        source_gpkg = get_test_file_copy_path('source_pk.gpkg')
+
+        # gpkg = get_test_file_copy_path('bd_pk.gpkg')
+        pg_layer = get_qgis_pg_layer(PG_BD_1, 'tipo_regla', truncate=True)
+        self.assertTrue(pg_layer.isValid())
+        self.assertEqual(pg_layer.featureCount(), 0)
+
+        input_layer_name = 'source'
+        input_layer = QgsVectorLayer("{}|layername={}".format(source_gpkg, input_layer_name), 'layer name', 'ogr')
+        self.assertTrue(input_layer.isValid())
+
+        QgsProject.instance().addMapLayers([input_layer, pg_layer])
+
+        # Let's create a feature with T_Id=1, we'll update its
+        # corresponding feature (which has a T_Id=100) successfully
+        f = QgsFeature(pg_layer.fields())
+        f.setAttribute("T_Id", 1)
+        f.setAttribute("codigo", "R0001")
+        f.setAttribute("descripcion", "ABC")
+        self.assertTrue(pg_layer.dataProvider().addFeatures([f]))
+        self.assertEqual(pg_layer.featureCount(), 1)
+
+        # res = processing.run("etl_load:appendfeaturestolayer",
+        #                      {'SOURCE_LAYER': input_layer,
+        #                       'SOURCE_FIELD': None,
+        #                       'TARGET_LAYER': pg_layer,
+        #                       'TARGET_FIELD': None,
+        #                       'ACTION_ON_DUPLICATE': 0})  # No action
+        #
+        # self.assertEqual(res['TARGET_LAYER'].featureCount(), 3)
+        # self.assertEqual(res[APPENDED_COUNT], 2)
+        # self.assertIsNone(res[UPDATED_COUNT])  # These are None because ACTION_ON_DUPLICATE is None
+        # self.assertIsNone(res[SKIPPED_COUNT])
+        #
+        # res = processing.run("etl_load:appendfeaturestolayer",
+        #                      {'SOURCE_LAYER': input_layer,
+        #                       'SOURCE_FIELD': 'codigo',
+        #                       'TARGET_LAYER': pg_layer,
+        #                       'TARGET_FIELD': 'codigo',
+        #                       'ACTION_ON_DUPLICATE': 2})  # UPDATE
+        #
+        # self.assertEqual(res['TARGET_LAYER'].featureCount(), 3)
+        # self.assertEqual(res[APPENDED_COUNT], 0)
+        # self.assertEqual(res[UPDATED_COUNT], 3)
+        # self.assertIsNone(res[SKIPPED_COUNT])
+
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         print('INFO: Tear down TestTablePK')
-        self.plugin.unload()
+        drop_all_tables()
+        cls.plugin.unload()
 
 
 if __name__ == '__main__':
