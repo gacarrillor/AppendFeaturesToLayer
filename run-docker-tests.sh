2627040df74d6634fea96c06b0a35ac27e1f09c5
Germán Carrillo <gcarrillo@linuxmail.org>
2023-09-07 17:29:16 +0200

    [tests] Make sure PG is ready before running tests (borrowed from Jorge Useche @jusech5)

--- run-docker-tests.sh
+++ run-docker-tests.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
+set -e
+
+printf "Wait a moment while loading the PG database."
+for i in {1..15}
+do
+  if PGPASSWORD='pass1' psql -h postgres -U user1 -p 5432 -l &> /dev/null; then
+    break
+  fi
+  printf "\nAttempt $i..."
+  sleep 2
+done
+printf "\nPostgreSQL ready!\n"
+
 pushd /usr/src/
 xvfb-run nose2-3
-#xvfb-run nose2-3 tests.test_pg_source_simple_polygons
+#xvfb-run nose2-3 tests.test_pk
 #xvfb-run nose2-3 tests.test_table_table
 #xvfb-run nose2-3 tests.test_pg_table_pg_table
 #xvfb-run nose2-3 tests.test_table_table
